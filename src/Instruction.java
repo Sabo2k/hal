9db57f86e4e9afb91601598b32834526b041681d
Sabo2k <63151233+Sabo2k@users.noreply.github.com>
2021-05-25 17:29:44 +0200

    Update Instruction.java added comments, refactored

--- src/Instruction.java
+++ src/Instruction.java
@@ -1,11 +1,33 @@
 public class Instruction
 {
+    /**
+     * each line in the HAL-file has a line-number
+     */
     public int line;
+    
+    /**
+     * an instruction has a name (e.g. "ADDNUM")
+     */
     public String instructionname;
+    
+    /**
+     * some instructions take a operand and do something with it
+     */
     public Number operand;
+    
+    /**
+     * in case, the operand is an Integer
+     */
     public int i_operand;
+    
+    /**
+     * in case, the operand is a Float 
+     */
     public double f_operand;
-
+    
+    /**
+    * standard constructor sets everything to a deafult value
+    */
     public Instruction()
     {
         this.line = 0;
@@ -13,88 +35,60 @@ public class Instruction
         this.f_operand = 0;
         this.i_operand = 0;
     }
-
+    
+    /**
+     * this handles all instructions that are sensitive towards 
+     * Integers or Floats and sets the operand to the fitting
+     * variable
+     * The aim was to prevent casting while processing instructions
+     * and to save performance
+     */
     public void determineInstruction()
     {
         switch (this.instructionname)
         {
-            case "START": {
-                break;
-            }
-            case "STOP": {
-                break;
-            }
-            case "OUT": {
-                break;
-            }
-            case "IN": {
-                break;
-            }
-            case "LOAD": {
-                break;
-            }
-            case "LOADNUM": {
-                if (this.i_operand != 0) {
+            case "LOADNUM": 
+            {
+                if (this.i_operand != 0) 
+                {
                     this.f_operand = this.i_operand;
                 }
                 break;
             }
-            case "STORE": {
-
-                break;
-            }
-            case "JUMPNEG": {
-                break;
-            }
-            case "JUMPPOS": {
-
-                break;
-            }
-            case "JUMPNULL": {
-
-                break;
-            }
-            case "JUMP": {
-
-                break;
-            }
-            case "ADD": {
-                break;
-            }
-            case "ADDNUM": {
-                if (this.i_operand != 0) {
+            case "ADDNUM": 
+            {
+                if (this.i_operand != 0) 
+                {
                     this.f_operand = this.i_operand;
                 }
                 break;
             }
-            case "SUB": {
-                break;
-            }
-            case "MUL": {
-                break;
-            }
-            case "DIV": {
-                break;
-            }
-            case "MULNUM": {
-                if (this.i_operand != 0) {
+            case "MULNUM": 
+            {
+                if (this.i_operand != 0) 
+                {
                     this.f_operand = this.i_operand;
                 }
                 break;
             }
-            case "DIVNUM": {
-                if (this.i_operand != 0) {
+            case "DIVNUM": 
+            {
+                if (this.i_operand != 0) 
+                {
                     this.f_operand = this.i_operand;
                 }
                 break;
             }
-            case "SUBNUM": {
-                if (this.i_operand != 0) {
+            case "SUBNUM": 
+            {
+                if (this.i_operand != 0) 
+                {
                     this.f_operand = this.i_operand;
                 }
                 break;
             }
-            default: {
+            default: 
+            {
                 break;
             }
         }
